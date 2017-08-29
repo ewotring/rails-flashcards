d9d45709414fd04866c9794b652f54ec723dd69a
Gabriel Osborne <osbornegabriel@gmail.com>
2017-08-28 21:02:34 -0500

    Get users account creation working, fix current_user helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def current_user
-    @user ||= session[:user_id] if session[:user_id]
+    @user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def current_user_id
