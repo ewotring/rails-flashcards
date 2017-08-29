50a77924ec939aebb3e46cb13a697ab21efc770f
Gabriel Osborne <osbornegabriel@gmail.com>
2017-08-28 20:34:30 -0500

    Get game logic working, yay

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,10 +1,10 @@
 class GuessesController < ApplicationController
 
-# get '/cards/:card_id/guesses/:id' do
-#   @card = Card.find(params[:card_id])
-#   @guess = @card.guesses.find(params[:id])
-#   erb :'cards/show'
-# end
+  def show
+    @card = Card.find(params[:card_id])
+    @guess = Guess.find(params[:id])
+    render
+  end
 
 #updates the guess
   def update
@@ -14,21 +14,13 @@ class GuessesController < ApplicationController
     @round = Round.find(@guess.round_id)
     #add +1 to attempt, this might not work
     @guess.attempts += 1
-
-    p @guess.card.answer
-    p params[:guess][:answer].chomp
-
     if @guess.card.answer == params[:guess][:answer].chomp
       @guess.solved = true
     end
-
     @guess.save
 
     if @guess.solved == false
-      @attempted = true
-      p @attempted
-      p "Yeah, this ain't solved"
-      render :"cards/show"
+      redirect_to card_guess_path(@card.id, @guess.id)
     else
       p "redirecting"
       redirect_to deck_round_path(@card.deck.id, @round.id)
